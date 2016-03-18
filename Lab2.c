dadaf1a05182d3c158268f0954eea6d8fc4f78ef
George Juarez <jjuarez@albany.edu>
2016-03-18 12:40:40 -0400

    Update Lab2.c

--- Lab2.c
+++ Lab2.c
@@ -1,12 +1,17 @@
 // George Juarez - ICSI333 Lab #2
+/*
+  Assignment was to take a user's input and exchange for quarters, dimes, nickles, and pennies
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void){
   int inputNum, quarters, dimes, nickels, pennies;
+  // prompt user for input and error check
   printf("Enter an int between 0 and 99: ");
   scanf(" %d",&inputNum);
   if(inputNum < 0 || inputNum > 99) exit(0);
+  // begin making change
   quarters = inputNum / 25;
   inputNum = inputNum % 25;
   dimes = inputNum / 10;
